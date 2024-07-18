82c9f5ca2f3423b648cfdbf66c681bec7194452f
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-07-18 08:11:03 +0530

    tic tac toe

--- jagged.java
+++ jagged.java
@@ -1,9 +1,35 @@
-public class jagged {
-    char[][] TicTacToeBoard={
-        {X,O,X},
-        {O,X,O},
-        {X,O,X},
-        }
-    }
-    
+import java.util.Scanner;
 
+public class jagged {
+
+    public static void main(String[] args) {
+
+        // create a 3x3 matrix
+        char[][] TicTacToeBoard = {
+            {'X', 'O', 'X'},
+            {'O', 'X', 'O'},
+            {'X', 'O', 'X'}
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Initialize the board with user input
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Enter character for position (" + i + ", " + j + "): ");
+                TicTacToeBoard[i][j] = scanner.next().charAt(0);
+            }
+        }
+
+        // Print the board
+        System.out.println("Current Tic Tac Toe Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(TicTacToeBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
