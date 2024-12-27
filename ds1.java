ea9cf94927f64623eefcefdfcb7fab2917ff598a
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-27 23:28:14 +0530

    ..

--- ds1.java
+++ ds1.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 public class ds1 {
     public static void main (String[]args){
     Scanner sc =new Scanner(System.in);
+    int Array;
+    int Start=0;  int End=Array.length-1;
+    for(int i=0;i<Array.length;i++){
+        Array[i]=sc.nextInt();
+    }   
+    int temp;
+     temp=array[i];
+     array[i]=End;
+     End = temp;
     
     }
     
